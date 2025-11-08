e5d827761e1b54816cfa4b586f10924dcfa7fbf5
hajdubalazs101 <hajdub@kkszki.hu>
2025-11-08 12:51:39 +0100

    teszt1kész

---
+++ hajdub_test1/hajdub_test1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hajdub_test1
+{
+        public class Szamologep
+        {
+            public static int Osszead(int a, int b)
+            {
+                return a + b;
+            }
+
+            public static int Kivon(int a, int b)
+            {
+                return a - b;
+            }
+
+            public static int Szoroz(int a, int b)
+            {
+                return a * b;
+            }
+
+            public static int Oszt(int a, int b)
+            {
+                if (b == 0)
+                    throw new DivideByZeroException("Nullával nem lehet osztani!");
+                return a / b;
+            }
+            static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ hajdub_test1/hajdub_test1Tests/SzamologepTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hajdub_test1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hajdub_test1.Tests
+{
+    [TestClass()]
+    public class SzamologepTests
+    {
+        [TestMethod()]
+        public void OsszeadTest()
+        {
+            int eredmeny = Szamologep.Osszead(5, 3);
+            Assert.AreEqual(8, eredmeny);
+        }
+        [TestMethod()]
+        public void KivonTest()
+        {
+            int eredmeny = Szamologep.Kivon(10, 4);
+            Assert.AreEqual(6, eredmeny);
+        }
+        [TestMethod()]
+        public void SzorozTest()
+        {
+            int eredmeny = Szamologep.Szoroz(6, 7);
+            Assert.AreEqual(42, eredmeny);
+        }
+        [TestMethod()]
+        public void OsztTest()
+        {
+            int eredmeny = Szamologep.Oszt(20, 4);
+            Assert.AreEqual(5, eredmeny);
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Oszt_NullaTest()
+        {
+            Szamologep.Oszt(10, 0);
+        }
+
+    }
+}
