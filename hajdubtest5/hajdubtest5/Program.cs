17481a851bfab51a35c372d89b26f5dfc6e01446
hajdubalazs101 <hajdub@kkszki.hu>
2025-11-18 14:17:32 +0100

    teszt5kész

---
+++ hajdubtest5/hajdubtest5/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hajdubtest5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string szoveg = "A C# egy nagyon sokoldalú, modern, objektumorientált programozási nyelv.";
+
+            Console.WriteLine("Szöveg: " + szoveg);
+            Console.WriteLine("Szavak száma: " + SzavakSzama(szoveg));
+            Console.WriteLine("Leghosszabb szó: " + LeghosszabbSzo(szoveg));
+            Console.WriteLine("Rendezett szavak: " + string.Join(", ", RendezettSzavak(szoveg)));
+            Console.WriteLine("Van-e benne 'nyelv'? " + TartalmazE(szoveg, "nyelv"));
+            Console.WriteLine("Mely szavak kezdődnek nagybetűvel? " + string.Join(", ", NagybetusSzavak(szoveg)));
+            Console.WriteLine("Csak betűk a szövegből: " + CsakBetuk(szoveg));
+            Console.WriteLine("Szógyakoriság: " + string.Join(", ", SzoGyakorisag(szoveg)));
+        }
+
+        public static int SzavakSzama(string szoveg)
+        {
+            return szoveg.Split(new[] { ' ', ',', '.', '!' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static string LeghosszabbSzo(string szoveg)
+        {
+            return szoveg.Split(new[] { ' ', ',', '.', '!' }, StringSplitOptions.RemoveEmptyEntries)
+                         .OrderByDescending(s => s.Length)
+                         .FirstOrDefault() ?? "";
+        }
+
+        public static string[] RendezettSzavak(string szoveg)
+        {
+            return szoveg.Split(new[] { ' ', ',', '.', '!' }, StringSplitOptions.RemoveEmptyEntries)
+                         .OrderBy(s => s)
+                         .ToArray();
+        }
+
+        public static bool TartalmazE(string szoveg, string keresett)
+        {
+            return szoveg.ToLower().Contains(keresett.ToLower());
+        }
+
+        public static string[] NagybetusSzavak(string szoveg)
+        {
+            return szoveg.Split(new[] { ' ', ',', '.', '!' }, StringSplitOptions.RemoveEmptyEntries)
+                         .Where(s => char.IsUpper(s[0]))
+                         .ToArray();
+        }
+
+        public static string CsakBetuk(string szoveg)
+        {
+            return new string(szoveg.Where(char.IsLetter).ToArray());
+        }
+
+        public static Dictionary<string, int> SzoGyakorisag(string szoveg)
+        {
+            return szoveg.ToLower()
+                         .Split(new[] { ' ', ',', '.', '!' }, StringSplitOptions.RemoveEmptyEntries)
+                         .GroupBy(s => s)
+                         .ToDictionary(g => g.Key, g => g.Count());
+        }
+    }
+}
---
+++ hajdubtest5/hajdubtest5Tests/ProgramTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hajdubtest5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hajdubtest5.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void SzavakSzamaTest()
+        {
+            string szoveg = "Ez egy teszt szöveg";
+            int eredmeny = Program.SzavakSzama(szoveg);
+            Assert.AreEqual(4, eredmeny);
+        }
+        [TestMethod()]
+        public void LeghosszabbSzoTest()
+        {
+            string szoveg = "Ez egy teszt szöveg";
+            string eredmeny = Program.LeghosszabbSzo(szoveg);
+            Assert.AreEqual("szöveg", eredmeny);
+        }
+        [TestMethod()]
+        public void TartalmazETest()
+        {
+            string szoveg = "Ez egy teszt szöveg";
+            bool eredmeny = Program.TartalmazE(szoveg, "teszt");
+            Assert.IsTrue(eredmeny);
+
+        }
+        [TestMethod()]
+        public void CsakBetukTest()
+        {
+            string szoveg = "Ez! egy teszt szöveg 123";
+            string eredmeny = Program.CsakBetuk(szoveg);
+            Assert.AreEqual("Ezegytesztszöveg", eredmeny);
+        }
+        [TestMethod()]
+        public void NagybetusSzavakTest()
+        {
+            string szoveg = "Ez egy Teszt Szöveg";
+            string[] eredmeny = Program.NagybetusSzavak(szoveg);
+            CollectionAssert.AreEqual(new string[] { "Ez", "Teszt", "Szöveg" }, eredmeny);
+        }
+        [TestMethod()]
+        public void RendezettSzavakTest()
+        {
+            string szoveg = "Ez egy teszt szöveg";
+            string[] eredmeny = Program.RendezettSzavak(szoveg);
+            var vvart = new string[] { "egy", "Ez", "szöveg", "teszt" };
+            CollectionAssert.AreEqual(vvart, eredmeny);
+        }
+        [TestMethod()]
+        public void SzoGyakorisagTest()
+        {
+            string szoveg = "teszt szöveg teszt";
+            var eredmeny = Program.SzoGyakorisag(szoveg);
+            var elvart = new Dictionary<string, int>
+            {
+                { "teszt", 2 },
+                {"szöveg", 1 }
+                
+            };
+            CollectionAssert.AreEquivalent(elvart, eredmeny);
+        }
+        
+    }
+    }
